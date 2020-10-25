e59bf8ab844d66e3d2a51c2cd4e8d61bc9edc48c
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-10-25 08:24:52 +0200

    update project

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,36 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
 
-class App extends StatelessWidget {
-  @override
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  void fitchImage() {
+    setState(() async {
+      counter++;
+      var response =
+          await get('https://jsonplaceholder.typicode.com/photos/$counter');
+      var imageModle = new ImageModel.fromJson(json.decode(response.body));
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Let\'s See Images!'),
         ),
-        body: Text('0'),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: null,
+          onPressed: fitchImage,
         ),
       ),
     );
